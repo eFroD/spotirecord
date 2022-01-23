490ba6c253c97c928e3fe016107769f1fcb41dc9
Frodl, Eric <eric.frodl@stud.tu-darmstadt.de>
2022-01-23 17:31:43 +0100

    add light controls

---
+++ spotirecord/lights/lights.py
@@ -0,0 +1,36 @@
+"""This module takes care of the light controls by realizing it as a class"""
+import time
+
+from rpi_ws281x import Adafruit_NeoPixel, ws, Color  # important: this module needs to run on a raspberry pi
+from spotirecord.config import read_config
+
+
+class LightController:
+    def __init__(self):
+        light_conf = read_config()["light"]
+        self.led_count = light_conf["led_count"]
+        self.max_brightness = light_conf["led_brightness"]
+        self.strip = Adafruit_NeoPixel(self.led_count, 18, 800000, 10, False, self.max_brightness, 0,
+                                  ws.SK6812_STRIP_RGBW)
+        self.strip.begin()
+
+    def fade_in(self, color, wait_time_ms=10):
+        """Shows the desired color after fading it in.
+
+        Args:
+            color: a tuple with the values (R, G, B)
+            wait_time_ms: the amount of milliseconds to wait until increasing the brightness.
+        """
+        self.cleanup()
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, Color(color[0], color[1], color[2]))
+        for i in range(self.max_brightness):
+            self.strip.setBrightness(i)
+            self.strip.show()
+            time.sleep(wait_time_ms/1000.0)
+
+    def cleanup(self):
+        """Turns off all LEDs of the stripe."""
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, Color(0, 0, 0))
+        self.strip.show()
